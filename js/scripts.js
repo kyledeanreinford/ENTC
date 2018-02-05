4d157cbcd13ff1dc6fa6688c9cd5dd822488e8ea
kyle dean reinford <kyledeanreinford@gmail.com>
2018-02-05 15:56:30 -0600

    Added social icons, background opacity layer

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Change nav background color on nav on scroll
+// document.body.onscroll = function changeNavBackground() {
+//   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//     console.log("scrolling");
+//     document.getElementById('nav').className = "navOnScroll";
+//   };
+// }
