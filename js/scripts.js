9e92456a7b08cf0e9748b8611672732f75886b12
kyle dean reinford <kyledeanreinford@gmail.com>
2018-02-06 11:33:27 -0600

    Updated mobile compatibility

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Change nav background color on nav on scroll
 document.body.onscroll = function changeNavBackground() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById('nav').className = "navOnScroll";
   } else {
     document.getElementById('nav').className = "";
