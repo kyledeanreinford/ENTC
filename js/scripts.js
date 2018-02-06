5cf9d27cab53aad46865705da8124776748bdb99
kyle dean reinford <kyledeanreinford@gmail.com>
2018-02-05 21:44:16 -0600

    Added scroll transparency, forms. Finished?

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 // Change nav background color on nav on scroll
-// document.body.onscroll = function changeNavBackground() {
-//   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-//     console.log("scrolling");
-//     document.getElementById('nav').className = "navOnScroll";
-//   };
-// }
+document.body.onscroll = function changeNavBackground() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById('nav').className = "navOnScroll";
+  } else {
+    document.getElementById('nav').className = "";
+  };
+}
